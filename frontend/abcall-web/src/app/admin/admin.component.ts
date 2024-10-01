ee6783e7c6ec55c7fe45d7268465e7b4aa8b336f
Esneider Velandia Gonzalez <e.velandia2164@uniandes.edu.co>
2024-09-30 21:23:19 -0500

    Add admin panel

--- frontend/abcall-web/src/app/admin/admin.component.ts
+++ frontend/abcall-web/src/app/admin/admin.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
+  showPlan: boolean = false;
+  showCreateAgent: boolean = false;
+  showBack: boolean = false;
+  showMenu: boolean = true;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showPlanComponent() {
+    this.showPlan = true;
+    this.showBack = true;
+    this.showCreateAgent = false;
+    this.showMenu = false
+  }
+
+  showCreateAgentComponent() {
+    this.showPlan = false;
+    this.showBack = true;
+    this.showCreateAgent = true;
+    this.showMenu = false;
+  }
+
+  showBackOption() {
+    this.showPlan = false;
+    this.showCreateAgent = false;
+    this.showBack = false;
+    this.showMenu = true;
   }
 
 }
