6042ed9ec849184ecd93e6890ddac627fc1627cc
Fabian Payan <f.payan@uniandes.edu.co>
2024-10-19 20:17:48 -0500

    URL updated to communicate with Production APIGateway

--- mobile/app/src/main/java/com/uniandes/abcall/data/service/ApiClient.kt
+++ mobile/app/src/main/java/com/uniandes/abcall/data/service/ApiClient.kt
@@ -8,8 +8,8 @@ import java.sql.Timestamp
 //private const val USERS_URL = "https://ms-users-719013485218.us-central1.run.app/"
 //private const val INCIDENTS_URL = "https://ms-incidents-719013485218.us-central1.run.app/"
 
-private const val USERS_URL = "http://10.0.2.2:9876/"
-private const val INCIDENTS_URL = "http://10.0.2.2:9877/"
+private const val USERS_URL = "https://misw-2024-api-gateway-pf-96b62bvm.uc.gateway.dev/"
+private const val INCIDENTS_URL = "https://misw-2024-api-gateway-pf-96b62bvm.uc.gateway.dev/"
 
 //private const val BASE_URL = "https://heroku-vinyls-g8-d9e277b35953.herokuapp.com/"
 
