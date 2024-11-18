492ee6d9fa59bff41d0d2ff499b00255bb5acb50
Esneider Velandia Gonzalez <e.velandia2164@uniandes.edu.co>
2024-11-18 18:16:50 -0500

    Update unit tests

--- frontend/abcall-web/src/app/app.service.spec.ts
+++ frontend/abcall-web/src/app/app.service.spec.ts
@@ -1,11 +1,13 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 import { AppService } from './app.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('Service: App', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [AppService]
     });
   });
